63f9f3177c1058d1456c413b9dc5a26a430ee5d5
TILTL <131395335+TILTL@users.noreply.github.com>
2023-06-23 12:27:49 -0400

    Add files via upload

---
+++ constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+    APP_INDEX = "app"
+    USER_INDEX = "user"
+    ES_URL = "http://10.182.0.2:9200"
+    ES_USERNAME = "gyhsnbb368"
+    ES_PASSWORD = "gyh666"
+    STRIPE_API_KEY = "sk_test_51NCqDyAUfLXvfS1r02AArEuzqBcZ3blgaxSS8WEonqgzXnScc1RAmqLrDzvKaz0NDyGI57coya9IaAn5UH73L3wG007IfjKLLG"
+    GCS_BUCKET = "gyhsnbb368-bucket"
+)
